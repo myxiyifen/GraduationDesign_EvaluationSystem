70ec4a8ed00fe069158bdebcfcffe9a85ea7f680
FeelingsOfMonkey <969068562@qq.com>
2018-05-13 16:29:42 +0800

    reset

--- code/GDES/GDES-SERVICE/src/main/java/com/gdes/GDES/service/MajorService.java
+++
@@ -1,9 +0,0 @@
-package com.gdes.GDES.service;
-
-import com.gdes.GDES.model.Major;
-
-import java.util.List;
-
-public interface MajorService {
-    public List<Major> queryByMajorId(String mid) throws Exception;
-}
--- code/GDES/GDES-SERVICE/src/main/java/com/gdes/GDES/service/ProfessionalabilitypointrequirementsService.java
+++
@@ -1,9 +0,0 @@
-package com.gdes.GDES.service;
-
-import com.gdes.GDES.model.Professionalabilitypointrequirements;
-
-import java.util.List;
-
-public interface ProfessionalabilitypointrequirementsService {
-    public List<Professionalabilitypointrequirements> queryByMajorBId(Integer mbid) throws Exception;
-}
